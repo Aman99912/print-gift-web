27afeee349011c2b38ab35661e214cf02814e260
Aman99912 <98696576+Aman99912@users.noreply.github.com>
2024-05-09 21:35:19 +0530

    Add files via upload

---
+++ product-link.js
@@ -0,0 +1,37 @@
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function picture() {
+    window.location.href = 'https://example.com';
+}
+function mug() {
+    window.location.href = 'https://www.w3schools.com/jsref/jsref_switch.asp';
+}
+function magicMug() {
+    window.location.href = 'https://example.com';
+}
+function colorMug() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+function redirectToLink() {
+    window.location.href = 'https://example.com';
+}
+
